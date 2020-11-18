91a67dd91ca5cc872aa57fc3bc51b56fea95985d
ciantfv456 <ciantfv456@gmail.com>
2020-11-18 15:15:11 +0200

    fix expand

--- init.js
+++ init.js
@@ -130,7 +130,7 @@ function onClick(e) {
                 && clicY >= y*height && clicY <= y*height+height-seperate) {
                     switch (e.button) {
                         case 0:
-                            squars[x][y].show = true;
+                            expand(squars,x,y);
                             break;
                         case 4:
                             squars[x][y].question = true;
@@ -153,14 +153,30 @@ function expand(board,x,y)
     if(board[x][y].number == 0)
     {
         board[x][y].show = true;
-        if(board[x+1][y].show === false && board[x+1][y] != -1)
-            board[x+1][y] == 0 ? expand(board,x+1,y) : board[x+1][y].show = true;
-        if(board[x-1][y].show === false && board[x-1][y] != -1)
-            board[x-1][y] == 0 ? expand(board,x-1,y) : board[x-1][y].show = true;
-        if(board[x][y+1].show === false && board[x][y+1] != -1)
-            board[x][y+1] == 0 ? expand(board,x,y+1) : board[x][y+1].show = true;
-        if(board[x][y-1].show === false && board[x][y-1] != -1)       
-            board[x][y-1] == 0 ? expand(board,x,y-1) : board[x][y-1].show = true;
+    
+        if(x+1 < x_squares && board[x+1][y].show === false)
+            expand(board,x+1,y);
+        if(y+1 < y_squares && board[x][y+1].show === false)
+            expand(board,x,y+1);
+        if(x-1 >= 0 && board[x-1][y].show === false)
+            expand(board,x-1,y);        
+        if(y-1 >= 0 && board[x][y-1].show === false)
+            expand(board,x,y-1);        
+        if(x+1 < x_squares && y+1 < y_squares && board[x+1][y+1].show === false)
+            expand(board,x+1,y+1);        
+        if(x-1 >= 0 && y-1 >= 0 && board[x-1][y-1].show === false)
+            expand(board,x-1,y-1);        
+        if(x+1 < x_squares && y-1 >= 0 && board[x+1][y-1].show === false)
+            expand(board,x+1,y-1);        
+        if(x-1 >= 0 && y+1 < y_squares && board[x-1][y+1].show === false)
+            expand(board,x-1,y+1);
+
+
+
+
+
+
+
     }
     else if(board[x][y] == -1)
     {
